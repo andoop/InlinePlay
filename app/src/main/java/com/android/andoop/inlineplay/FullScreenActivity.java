f3aeda916cc0229596ca13696739f8e2528e9cbb
黄洞洞 <huangdongdong@zhihu.com>
2019-04-04 16:26:06 +0800

    添加 全屏支持

--- app/src/main/java/com/android/andoop/inlineplay/FullScreenActivity.java
+++ app/src/main/java/com/android/andoop/inlineplay/FullScreenActivity.java
@@ -30,6 +30,7 @@ public class FullScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen);
+
         videoView = findViewById(R.id.video_view);
         Bundle extras = getIntent().getExtras();
         dataSource = extras.getParcelable(KEY_DATASOURCE);
